7219e81500a35ba885eca1346f76a21c87a289f7
Robin C. Ladiges <rcl.git@blackpinguin.de>
2023-10-15 23:33:22 +0200

    repair compilation with GS v3.16

---
+++ SolInEx_EN/PrjGothic/Externals.d
@@ -0,0 +1,5 @@
+// add function prototypes here
+func int ai_printscreen(var string par0, var int par1, var int par2, var string par3, var int par4);
+func void ai_snd_play(var __class par0, var string par1);
+func void ai_createiteminslot(var __class par0, var string par1, var int par2);
+func void ai_removeitemfromslot(var __class par0, var string par1);
